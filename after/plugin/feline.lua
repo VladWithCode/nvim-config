8dd665d08b0c4453183a6b52723461696f412f3e
Bladimir Cabrera <vladwithcode@gmail.com>
2024-10-12 12:58:00 -0600

    change: feline styles for kitty

--- after/plugin/feline.lua
+++ after/plugin/feline.lua
@@ -2,13 +2,44 @@ local feline = require('feline')
 local vi_mode = require('feline.providers.vi_mode')
 local file = require('feline.providers.file')
 
-local baseSep = function(fg, bg)
+local baseSep = function(fg, bg, str)
     fg = fg or 'none'
     bg = bg or 'none'
+    str = str or ' '
     return {
         always_visible = true,
         str = ' ',
         hl = { fg = fg, bg = bg },
+    }
+end
+
+local sep_arrow_right = function(next_bg)
+    next_bg = next_bg or 'none'
+    return {
+        provider = '',
+        hl = { fg = 'none', bg = 'none' },
+        right_sep = {
+            always_visible = true,
+            str = 'right_filled',
+            hl = { fg = 'bg', bg = next_bg }
+        }
+    }
+end
+
+local sep_slant = function(next_bg, lr, v)
+    next_bg = next_bg or 'none'
+    lr = lr or 'left'
+    v = v or ''
+    return {
+        provider = '',
+        hl = { fg = 'none', bg = 'none' },
+        right_sep = {
+            always_visible = true,
+            str = function ()
+                return string.format('slant_%s%s', lr, v)
+            end,
+            hl = { fg = 'bg', bg = next_bg }
+        }
     }
 end
 
@@ -19,9 +50,18 @@ local c = {
             return string.format('%s', vi_mode.get_vim_mode())
         end,
         hl = function()
-            return { fg = vi_mode.get_mode_color(), bg = 'none' }
+            local fg = vi_mode.get_mode_color()
+            if vi_mode.get_vim_mode() == 'INSERT' then
+                fg = 'purple'
+            end
+            return { fg = fg, bg = 'blue' }
         end,
-        right_sep = baseSep(),
+        left_sep = baseSep('none', 'blue'),
+        right_sep = {
+            always_visible = true,
+            str = 'right_filled',
+            hl = { bg = 'bg', fg = 'blue' }
+        },
     },
 
     file_name = {
@@ -32,8 +72,17 @@ local c = {
                 type = 'relative',
             },
         },
-        hl = { fg = '#5F63B4', bg = 'none' },
-        right_sep = baseSep(),
+        hl = { fg = 'blue', bg = 'cyan' },
+        left_sep = {
+            str = 'right_filled',
+            always_visible = true,
+            hl = { fg = 'cyan', bg = 'cyan' }
+        },
+        right_sep = {
+            str = 'right_filled',
+            always_visible = true,
+            hl = { fg = 'cyan', bg = 'bg' }
+        },
     },
 
     git_branch = {
@@ -48,7 +97,17 @@ local c = {
             end
             return s
         end,
-        hl = { fg = '#AF9A91', bg = 'none' },
+        hl = { fg = 'white', bg = 'red' },
+        left_sep = {
+            always_visible = true,
+            str = 'left_rounded',
+            hl = { fg = 'red', bg = 'bg' }
+        },
+        right_sep = {
+            always_visible = true,
+            str = 'right_filled',
+            hl = { fg = 'red', bg = 'bg' }
+        }
     },
 
     -- right
@@ -63,14 +122,12 @@ local c = {
             end
             return s
         end,
-        hl = { fg = '#D6B04E', bg = 'none' },
-        right_sep = baseSep(),
+        hl = { fg = 'purple' },
     },
 
     search_count = {
         provider = 'search_count',
-        hl = { fg = '#E2C2BB', bg = 'none' },
-        right_sep = baseSep(),
+        hl = { fg = 'white' },
     },
 
     cursor_position = {
@@ -78,8 +135,17 @@ local c = {
             name = 'position',
             opts = { padding = true },
         },
-        hl = { fg = '#F66813', bg = 'none' },
-        right_sep = baseSep(),
+        hl = { fg = 'yellow', bg = 'blue' },
+        left_sep = {
+            always_visible = true,
+            str = 'slant_left',
+            hl = { fg = "blue" }
+        },
+        right_sep = {
+            always_visible = true,
+            str = 'slant_right_2',
+            hl = { fg = "blue" }
+        },
     },
 
     scroll_bar = {
@@ -87,16 +153,23 @@ local c = {
             name = 'scroll_bar',
             opts = { reverse = true },
         },
-        hl = { fb = '#8086EF', bg = 'none' },
-    }
+        hl = { fg = 'green', bg = 'bg' },
+    },
+
+    -- Utility
+    transparent = {
+        provider = '',
+        hl = { fg = 'none', bg = 'none' }
+    },
 }
 
 local active = {
     { -- left
         c.vim_mode,
+        sep_arrow_right('cyan'),
         c.file_name,
+        { provider = ' ', hl = { fg = 'none' } },
         c.git_branch,
-        "",
     },
     { -- right
         c.macro,
@@ -122,12 +195,32 @@ local inactive = {
     }
 }
 
+local theme = {
+    black = '#001626',
+    blue = '#003052',
+    red = '#FF4E52',
+    green = '#9ECC6E',
+    yellow = '#E4D093',
+    lightblue = '#80ACF8',
+    purple = '#C993E4',
+    cyan = '#78DBCB',
+    white = '#EBF7FF',
+    bg = '#001626',
+    fg = '#EBF7FF',
+}
+
+local sep = {
+    right_chev = '❰',
+    left_chev = '❱',
+}
+
 local opts = {
     components = {
         active = active,
         inactive = inactive,
-    }
+    },
+    separators = sep,
 }
 
 feline.setup(opts)
-print("wtf")
+feline.use_theme(theme)
